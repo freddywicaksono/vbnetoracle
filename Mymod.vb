0da6eb58d70880b0d812e6372bf13ed3ccd4af47
Freddy Wicaksono <59552422+freddywicaksono@users.noreply.github.com>
2020-12-23 20:36:02 +0700

    Add files via upload

---
+++ Mymod.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Data.OracleClient
+Module Mymod
+    Public mycommand As New System.Data.OracleClient.OracleCommand
+    Public myadapter As New System.Data.OracleClient.OracleDataAdapter
+    Public mydata As New DataTable
+    Public DR As System.Data.OracleClient.OracleDataReader
+    Public SQL As String
+    Public conn As New System.Data.OracleClient.OracleConnection
+    Public cn As New Connection
+
+    
+    'Tabel Fakultas
+    '-------------------------------
+    Public fakultas_baru As Boolean
+    Public ofakultas As New Fakultas
+    '--------------------------------
+
+    Public Sub DBConnect()
+        cn.DataSource = "xe"
+        cn.UserID = "freddy"
+        cn.Password = "123"
+        cn.Connect()
+    End Sub
+
+    Public Sub DBDisconnect()
+        cn.Disconnect()
+    End Sub
+End Module
